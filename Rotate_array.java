689a52d8c694b8926bdb267503ea262c494a9795
rishushashank <123060785+rishushashank@users.noreply.github.com>
2023-01-20 22:00:01 +0530

    Add files via upload

---
+++ Rotate_array.java
@@ -0,0 +1,23 @@
+public class Rotate_array {
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int k=3;
+        if(k > arr.length)
+        return;
+        int ans[] = new int[arr.length];
+        int j=0;
+        /* 1>Intermediate array
+        
+        for(int i=arr.length-k;i<arr.length;i++){
+            ans[j++] = arr[i]; 
+        }
+        for(int i=0;i<arr.length-k;i++){
+            ans[j++] = arr[i];
+        }
+        for(int i=0;i<arr.length;i++)
+        System.out.print(ans[i]+" ");
+         */ 
+
+
+    }
+}
