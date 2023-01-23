fcf2ba5ca89ba8238f673aed89d081dd411735ca
rishushashank <123060785+rishushashank@users.noreply.github.com>
2023-01-23 14:12:05 +0530

    Add files via upload

---
+++ multiThreding.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+// class mythread extends Thread{
+// public void run(){
+//     int i=0;
+//         while(true){
+//             System.out.println(i++);
+//         }
+// }
+// }
+// public class multiThreding {
+//     public static void main(String[] args){
+//         mythread t= new mythread();
+//         t.start();
+//         int i=0;
+//         while(true){
+//             System.out.println(i +"main");
+//             i++;
+//         }
+//     }
+// }
+
+
+
+// public class multiThreding extends Thread{
+    
+//     public void run(){
+//         int i=0;
+//         while(true){
+//             System.out.println(i++);
+//         }
+//     }
+//     public static void main(String[] args){
+//         multiThreding t = new multiThreding();
+//         t.start();
+//         int i=0;
+//         while(true){
+//             System.out.println(i+"main");
+//             i++;
+//         }
+//     }
+
+// }
+
+
+
+
+
+// public class multiThreding implements Runnable{
+//     public void run(){
+//         int i=0;
+//         while(true){
+//             System.out.println(i++);
+//         }
+//     }
+//     public static void main(String args[]){
+//         multiThreding m = new multiThreding();
+//         Thread t = new Thread(m);
+//         t.start();;
+//         int i=0;
+//         while(true){
+//             System.out.println(i+"main");
+//             i++;
+//         }
+//     }
+// }
+
+
+
+
+// class thread implements Runnable{
+//     public void run(){
+//         int i=0;
+//         while(true){
+//             System.out.println(i++);
+//         }
+//     }
+// }
+// public class multiThreding {
+   
+//     public static void main(String args[]){
+//         thread m = new thread();
+//         Thread t = new Thread(m);
+//         t.start();
+//         int i=0;
+//         while(true){
+//             System.out.println(i+"main");
+//             i++;
+//         }
+//     }
+// }
+
+
+
+
+
